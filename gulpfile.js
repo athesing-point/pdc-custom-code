c34134bc2aa0ea5cdd58fd4a696350744f805ea8
Austin Thesing <athesing@point.com>
2025-05-13 21:28:38 -0600

    feat: add babel support for ES modules in build process

--- gulpfile.js
+++ gulpfile.js
@@ -5,16 +5,24 @@ const terser = require("gulp-terser");
 const browserify = require("browserify");
 const source = require("vinyl-source-stream");
 const buffer = require("vinyl-buffer");
-const glob = require("glob");
 const path = require("path");
 const through2 = require("through2");
+const babelify = require("babelify");
 
 // Process all JS files with browserify
 function bundleJS() {
   // Find all JS files except those in node_modules, min directory, and gulpfile.js
   return through2.obj((file, enc, next) => {
     browserify(file.path)
+      .transform(babelify, {
+        presets: ["@babel/preset-env"],
+        sourceMaps: false,
+      })
       .bundle()
+      .on("error", function (err) {
+        console.error(err.message);
+        this.emit("end");
+      })
       .pipe(source(path.basename(file.path)))
       .pipe(buffer())
       .pipe(terser())
